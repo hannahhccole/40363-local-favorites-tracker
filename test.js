1f9919f21ddc9d8d6f59f318e985aba5dbbff761
hannahhccole <hannah.cole@tcu.edu>
2025-10-23 17:23:22 -0500

    Commit #2 10/23
    Lab 14 Complete JS

---
+++ test.js
@@ -0,0 +1,8 @@
+// Does "Pizza Place" contain "Pizza"?
+let restaurantName = "Pizza Place";
+let searchTerm = "Place";
+ 
+let found = restaurantName.includes(searchTerm);
+ 
+console.log(found);
+// Prints: true
